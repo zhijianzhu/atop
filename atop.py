4fa5fe64a4ea43e170125509c7dc5c5cfe7b4297
atop <wu_chao@bah.com>
2020-03-06 22:05:45 -0500

    first web page

---
+++ atop.py
@@ -0,0 +1,33 @@
+# Ref: https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-16-04
+
+import datetime
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import make_response
+
+import sqlite3
+from flask import g
+import pandas as pd
+
+from flask_basicauth import BasicAuth
+
+#import sys
+#sys.path.append('../')
+
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+
+es = Elasticsearch('localhost', port=9200)
+
+
+
+@app.route("/")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
