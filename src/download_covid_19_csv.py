22252930bd8c70a0061c7b85f621e988c0650caf
atop <wu_chao@bah.com>
2020-03-27 21:36:55 -0400

    update download covid script

--- src/download_covid_19_csv.py
+++ src/download_covid_19_csv.py
@@ -5,7 +5,8 @@ print("downloading confirmed case")
 base_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
 
 data_file = 'time_series_19-covid-Confirmed.csv'
-url = base_url + data_file
+data_file_1 = 'time_series_covid19_confirmed_global.csv'
+url = base_url + data_file_1
 new_file = "data/" + data_file
 
 if os.path.exists(new_file):
@@ -13,7 +14,9 @@ if os.path.exists(new_file):
 wget.download(url, new_file)
 
 data_file = 'time_series_19-covid-Deaths.csv'
-url = base_url + data_file
+data_file_1 = 'time_series_covid19_deaths_global.csv'
+
+url = base_url + data_file_1
 new_file = "data/" + data_file
 
 if os.path.exists(new_file):
@@ -21,7 +24,9 @@ if os.path.exists(new_file):
 wget.download(url, new_file)
 
 data_file = 'time_series_19-covid-Recovered.csv'
-url = base_url + data_file
+data_file_1 = 'time_series_covid19_recovered_global.csv'
+
+url = base_url + data_file_1
 new_file = "data/" + data_file
 
 if os.path.exists(new_file):
